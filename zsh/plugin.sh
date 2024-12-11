657853bd692d9b3d64b0d209eb92e5c522e83d7f
Adylan Roaffa <adylan.dev@gmail.com>
2024-12-11 22:36:19 +0800

    feat: add zsh plugins
    feat: rename tmux
    
    feat: always start on maximized

---
+++ zsh/plugin.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions ??(Wed,Dec11)??
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions.git ~/.oh-my-zsh/custom/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search.git ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
